5d8e817b547d49d7311e3768278e5d3d75b1d4b4
yallknow <ignites_you@protonmail.com>
2025-10-03 15:22:05 +0300

    [NEW] (all) Added JsonCpp library

--- conanfile.py
+++ conanfile.py
@@ -8,5 +8,6 @@ class ExampleRecipe(ConanFile):
     def requirements(self):
         self.requires("boost/1.88.0")
         self.requires("box2d/3.1.0")
+        self.requires("jsoncpp/1.9.6")
         self.requires("imgui/1.91.8-docking", override=True)
         self.requires("imgui-sfml/2.6.1")
